fe6e6083833d748d6716a0cd11306f6780b79618
Ludwig <loic.frueh@gmail.com>
2018-12-04 09:49:28 +0100

    Now it works

--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/MainActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/MainActivity.java
@@ -26,7 +26,6 @@ package ch.heigvd.iict.sym.a3dcompassapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/QRActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/QRActivity.java
@@ -1,12 +1,10 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
-<<<<<<< HEAD
+
 import android.support.v7.app.AppCompatActivity;
 
 public class QRActivity extends AppCompatActivity {
 
 
-=======
-class QRActivity {
->>>>>>> c67126517bd14f0b32994637193e046749292ae9
+
 }
