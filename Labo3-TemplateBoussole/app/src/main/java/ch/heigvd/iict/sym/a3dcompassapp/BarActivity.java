127b218185df59f09f58b9ab551815c083a2f771
zedsdead95 <mr.walid.koubaa@gmail.com>
2018-12-04 11:44:53 +0100

    Code bar and code qr working in the same activity

--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarActivity.java
@@ -1,9 +1,15 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class BarActivity  extends AppCompatActivity {
 
@@ -15,14 +21,30 @@ public class BarActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_bar);
 
         scanningButton = findViewById(R.id.button_bar);
+        final Activity activity = this;
 
 
         scanningButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                new IntentIntegrator(activity).initiateScan(); // `this` is the current Activity
             }
         });
+    }
+
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
