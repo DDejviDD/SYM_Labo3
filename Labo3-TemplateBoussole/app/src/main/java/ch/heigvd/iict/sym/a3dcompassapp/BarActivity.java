ea0cf002fb2393c04abd58cfef4096502a387235
Ludwig <loic.frueh@gmail.com>
2018-12-04 10:15:50 +0100

    Navigation works, I'm the best

--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarActivity.java
@@ -2,7 +2,7 @@ package ch.heigvd.iict.sym.a3dcompassapp;
 
 import android.support.v7.app.AppCompatActivity;
 
-public class BarActivity  extends AppCompatActivity{
+public class BarActivity extends AppCompatActivity {
 
     
 }
