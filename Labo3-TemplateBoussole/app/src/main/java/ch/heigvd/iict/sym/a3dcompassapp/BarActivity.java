966b9a5615c3ec0f536f5e5f1c850c91547b2b1d
zedsdead95 <mr.walid.koubaa@gmail.com>
2018-12-04 10:19:38 +0100

    bar activity debigging

--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarActivity.java
@@ -1,8 +1,28 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class BarActivity  extends AppCompatActivity{
+public class BarActivity  extends AppCompatActivity implement{
 
-    
+    private Button scanningButton;
+
+    @Override
+    protected void onCreate(Bundle instanceState) {
+        super.onCreate(instanceState);
+        setContentView(android.R.layout.);
+
+        scanningButton = findViewById(R.id.button_bar);
+
+
+        scanningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }
